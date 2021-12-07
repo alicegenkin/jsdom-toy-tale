3927a1e391ccf3ffc18a59c6d083ba5b6bb3a279
Alice Genkin <agenkin1@gmail.com>
2021-12-07 11:32:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -45,9 +45,23 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function renderToys(toy) {
+let card = document.createElement('div')
+card.className = 'card'
 let h2 = document.createElement('h2')
-toyCollection.append(h2)
+//toyCollection.append(h2)
 h2.innerText = toy.name
+let img = document.createElement('img')
+///toyCollection.append(img)
+img.src = toy.image
+img.className = 'toy-avatar'
+let p = document.createElement('p')
+//toyCollection.append(p)
+img.innerText = toy.likes
+let button = document.createElement('button')
+button.innerText = 'Like'
+//toyCollection.append(button)
+card.append(h2, img, p, button)
+toyCollection.append(card)
 }
 
 let toyCollection = document.querySelector('#toy-collection')
